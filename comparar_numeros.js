d2f8884bcda1d3c5c2fa89536ed538181fa0303c
Jorge T <git.trrblnc@gmail.com>
2026-01-13 17:28:29 -0600

    review: verificacion de logica

--- comparar_numeros.js
+++ comparar_numeros.js
@@ -2,3 +2,12 @@ let numeros = [Number(prompt("Ingresa un numero: ")),
 Number(prompt("Ingresa otro numero: ")),
 Number(prompt("Ingresa un tercer numero: "))
 ];
+
+let ente = 0;
+let indice = 0;
+let rompebucle = false;
+
+do {
+console.log(numeros[indice]);
+indice++;
+} while (indice<numeros.length-1);
